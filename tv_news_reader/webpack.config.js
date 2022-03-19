f894eb9cf03de1aa1a25595c0e17ab0292518999
omid daghigh <sajad.daghigh@gmail.com>
2022-03-19 17:03:56 +0330

    dsfsdf

---
+++ tv_news_reader/webpack.config.js
@@ -0,0 +1,16 @@
+var nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+    target: "node",
+    externals: [nodeExternals()],
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.js&/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+}
