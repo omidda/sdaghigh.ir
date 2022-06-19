0390401433ae758aa8f64519f1dd9abb57daf554
Sajad Daghigh <sajad.daghigh@gmail.com>
2022-06-20 01:45:49 +0430

    mongoose

--- temp_test/server.js
+++ temp_test/server.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var mongoose = require('mongoose')
+
 var app = express()
 var bodyParser = require('body-parser')
 // create application/json parser
@@ -13,6 +15,25 @@ app.post('/chkpost', (req, res) => {
     console.log(JSON.stringify(req.body) + ' B O D Y ')
     res.sendStatus(200)
 })
+
+let mongoconstr = "mongodb://"+process.env.TV_MONGO_USER+
+":"+process.env.TV_MONGO_PASSWORD+
+"@"+process.env.TV_MONGO_HOST+":"+
+process.env.TV_MONGO_PORT+"/admin?w=majority"
+
+console.log(mongoconstr)
+
+mongoose.connect(mongoconstr, (err)=>{
+    if(err)
+    {
+        console.log(`Mongo DB not connected ${err}`)
+    }
+    else{
+        console.log(`Mongo DB is connected`)
+    }
+    
+})
+
 var server = app.listen(2000, () => {
     console.log("Server is listening to PORT",
         server.address().port);
