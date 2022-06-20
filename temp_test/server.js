7acf060945c5e4c319c4af8a171e480a0076264d
omid daghigh <sajad.daghigh@gmail.com>
2022-06-20 17:01:35 +0430

    confirmes

--- temp_test/server.js
+++ temp_test/server.js
@@ -7,19 +7,39 @@ var bodyParser = require('body-parser')
 app.use(express.static(__dirname));
 app.use(bodyParser.json())
 
+var LogMessage = mongoose.model("LogMessage", {
+    title : String,
+    message: String,
+    phoneNumber: Number
+})
+
 app.get("/chk", (req, res) => {
-    res.send('HELLO BABY')
+
+    LogMessage.find({"_id": "62b0000230d3c60226f2f7fe"},(err,data) => {
+        res.send(data)
+     })
+    
 })
  
 app.post('/chkpost', (req, res) => { 
     console.log(JSON.stringify(req.body) + ' B O D Y ')
+
+    var newMessage = new LogMessage(req.body)
+    newMessage.save((err) => {
+        if (err)
+            console.log("Ann error occured while saving")
+        else
+            console.log("Data have saved")
+     })
     res.sendStatus(200)
 })
 
-let mongoconstr = "mongodb://"+process.env.TV_MONGO_USER+
-":"+process.env.TV_MONGO_PASSWORD+
-"@"+process.env.TV_MONGO_HOST+":"+
-process.env.TV_MONGO_PORT+"/admin?w=majority"
+// let mongoconstr = "mongodb://"+process.env.TV_MONGO_USER+
+// ":"+process.env.TV_MONGO_PASSWORD+
+// "@"+process.env.TV_MONGO_HOST+":"+
+// process.env.TV_MONGO_PORT+"/admin?w=majority"
+
+let mongoconstr = "mongodb://localhost:27017"
 
 console.log(mongoconstr)
 
